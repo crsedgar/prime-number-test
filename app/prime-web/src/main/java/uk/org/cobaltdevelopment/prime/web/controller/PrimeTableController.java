ce3cadcb534dad8ed593591012d53f7bf8764eb1
Chris Edgar <crsedgar@hotmail.com>
2017-09-28 00:03:36 +0100

    canCreateTable - red

--- app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableController.java
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableController.java
@@ -2,7 +2,14 @@ package uk.org.cobaltdevelopment.prime.web.controller;
 
 import org.springframework.stereotype.Controller;
 
+import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
+
 @Controller
 public class PrimeTableController {
+
+	public TableDto generateTable(int numberOfPrimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableControllerTest.java
+++ app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableControllerTest.java
@@ -1,7 +1,12 @@
 package uk.org.cobaltdevelopment.prime.web.controller;
 
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+
+import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
 
 public class PrimeTableControllerTest {
 
@@ -11,7 +16,14 @@ public class PrimeTableControllerTest {
 
 	@Test
 	public void canCreateController() {
+		new PrimeTableController();
+	}
+
+	@Test
+	public void canCreateTable() {
 		PrimeTableController controller = new PrimeTableController();
+		TableDto t = controller.generateTable(2);
+		assertThat(t, Matchers.notNullValue());
 	}
 
 }
