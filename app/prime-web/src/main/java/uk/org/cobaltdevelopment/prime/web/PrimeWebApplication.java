2bee2b8e88058687e91ab711862611d2a29de8d8
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 21:39:17 +0100

    initial commit web app

---
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/PrimeWebApplication.java
@@ -0,0 +1,12 @@
+package uk.org.cobaltdevelopment.prime.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeWebApplication.class, args);
+	}
+}
---
+++ app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/PrimeWebApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.org.cobaltdevelopment.prime.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
