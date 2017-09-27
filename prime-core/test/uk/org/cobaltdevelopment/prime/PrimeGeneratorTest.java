dc71ef7f029cbf3055bcfc91f75140ba2cd8e4b7
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 11:27:17 +0100

    canGeneratorFirstPrime - red

--- prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
+++ prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class PrimeGeneratorTest {
@@ -16,7 +15,6 @@ public class PrimeGeneratorTest {
 		pg = new PrimeGenerator();
 	}
 
-	@Ignore
 	@Test
 	public void canGeneratorFirstPrime() {
 		assertThat(pg.generatePrime(1), equalTo(new int[] { 2 }));
@@ -32,39 +30,39 @@ public class PrimeGeneratorTest {
 		isPrime(2);
 		isNotPrime(4);
 	}
-	
+
 	@Test
-	public void testOneIsNotAPrime(){
+	public void testOneIsNotAPrime() {
 		isNotPrime(1);
 	}
-	
+
 	@Test
-	public void testSecondPrime(){
+	public void testSecondPrime() {
 		isPrime(3);
 	}
-	
+
 	@Test
-	public void testLargePrime(){
+	public void testLargePrime() {
 		isPrime(997);
 	}
-	
+
 	@Test
-	public void testPrimeLargerThanFiveEndingInFive(){
+	public void testPrimeLargerThanFiveEndingInFive() {
 		isNotPrime(505);
 		isPrime(5);
 	}
-	
+
 	@Test
-	public void testLargeNonPrime(){
+	public void testLargeNonPrime() {
 		isNotPrime(999);
 	}
-	
-	private void isPrime(int num){
+
+	private void isPrime(int num) {
 		assertThat(pg.isPrime(num), equalTo(true));
 	}
-	
-	private void isNotPrime(int num){
+
+	private void isNotPrime(int num) {
 		assertThat(pg.isPrime(num), equalTo(false));
-	}	
+	}
 
 }
