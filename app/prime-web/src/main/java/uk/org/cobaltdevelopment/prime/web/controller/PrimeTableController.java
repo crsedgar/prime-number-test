15374843545d0ccb01ffa05d2ac1a18f51558986
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 23:55:13 +0100

    canCreateController - green

---
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableController.java
@@ -0,0 +1,8 @@
+package uk.org.cobaltdevelopment.prime.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PrimeTableController {
+
+}
---
+++ app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableControllerTest.java
@@ -0,0 +1,17 @@
+package uk.org.cobaltdevelopment.prime.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrimeTableControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void canCreateController() {
+		PrimeTableController controller = new PrimeTableController();
+	}
+
+}
