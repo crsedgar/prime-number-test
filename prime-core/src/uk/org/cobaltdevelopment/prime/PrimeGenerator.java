34995e199ba63144c44eafff23e3145b6270a147
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 10:40:03 +0100

    canGeneratorFirstPrime - red

--- prime-core/src/uk/org/cobaltdevelopment/prime/PrimeGenerator.java
+++ prime-core/src/uk/org/cobaltdevelopment/prime/PrimeGenerator.java
@@ -2,4 +2,8 @@ package uk.org.cobaltdevelopment.prime;
 
 public class PrimeGenerator {
 
+	public int generatePrime(int numToGenerate) {
+		return 0;
+	}
+
 }
--- prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
+++ prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
@@ -1,12 +1,16 @@
 package uk.org.cobaltdevelopment.prime;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class PrimeGeneratorTest {
 
 	@Test
-	public void canCreateGenerator(){
+	public void canGeneratorFirstPrime() {
 		PrimeGenerator pg = new PrimeGenerator();
+		assertThat(pg.generatePrime(1), equalTo(new int[] { 2 }));
 	}
-	
+
 }
