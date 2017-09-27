b2e8465f05f070e01f639a05d6db09dc29ccd530
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 10:25:43 +0100

    initial commit
    initial commit test setup

---
+++ prime-core/src/uk/org/cobaltdevelopment/prime/PrimeGenerator.java
@@ -0,0 +1,5 @@
+package uk.org.cobaltdevelopment.prime;
+
+public class PrimeGenerator {
+
+}
---
+++ prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
@@ -0,0 +1,12 @@
+package uk.org.cobaltdevelopment.prime;
+
+import org.junit.Test;
+
+public class PrimeGeneratorTest {
+
+	@Test
+	public void testProjectSetup(){
+		
+	}
+	
+}
