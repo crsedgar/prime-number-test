bf32a0a97682f3153a15e40876bf9d7741cb6515
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 12:05:00 +0100

    canGenerateSmallestTable - green

--- prime-core/src/uk/org/cobaltdevelopment/prime/PrimeTable.java
+++ prime-core/src/uk/org/cobaltdevelopment/prime/PrimeTable.java
@@ -3,9 +3,27 @@ package uk.org.cobaltdevelopment.prime;
 public class PrimeTable {
 
 	private int[][] table;
-	
+
 	public PrimeTable(int[] primes) {
-		table = new int[primes.length+1][primes.length+1];
+		table = new int[primes.length + 1][primes.length + 1];
+
+		int primeCount = 1;
+		// populate outer cells
+		for (int i = 0; i < primes.length; i++) {
+			table[0][primeCount] = primes[i];
+			table[primeCount][0] = primes[i];
+			primeCount++;
+		}
+
+		// fill in multiplication
+		for (int i = 1; i < table.length; i++) {
+			for (int j = 1; j < table.length; j++) {
+				int rowPrime = table[i][0];
+				int colPrime = table[0][j];
+				table[i][j] = rowPrime * colPrime;
+			}
+		}
+
 	}
 
 	public int numberOfRows() {
