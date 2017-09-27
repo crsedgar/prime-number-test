994fedd23a70c25f00df7058a1eacbf8a53f43fc
Chris Edgar <crsedgar@hotmail.com>
2017-09-27 10:46:32 +0100

    testNonNaturalNumbersZero - red

--- prime-core/src/uk/org/cobaltdevelopment/prime/PrimeGenerator.java
+++ prime-core/src/uk/org/cobaltdevelopment/prime/PrimeGenerator.java
@@ -6,4 +6,8 @@ public class PrimeGenerator {
 		return 0;
 	}
 
+	public void isPrime(int i) {
+		
+	}
+
 }
--- prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
+++ prime-core/test/uk/org/cobaltdevelopment/prime/PrimeGeneratorTest.java
@@ -3,14 +3,22 @@ package uk.org.cobaltdevelopment.prime;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PrimeGeneratorTest {
 
+	@Ignore
 	@Test
 	public void canGeneratorFirstPrime() {
 		PrimeGenerator pg = new PrimeGenerator();
 		assertThat(pg.generatePrime(1), equalTo(new int[] { 2 }));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testNonNaturalNumbersZero(){
+		PrimeGenerator pg = new PrimeGenerator();
+		pg.isPrime(0);
 	}
 
 }
