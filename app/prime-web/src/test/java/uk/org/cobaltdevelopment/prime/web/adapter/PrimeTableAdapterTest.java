1d78434eed7edfbe051a6dfd266f7d81859353a2
Chris Edgar <crsedgar@hotmail.com>
2017-09-28 15:33:29 +0100

    canAdaptPrimeTableToDto - red

--- app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/adapter/PrimeTableAdapterTest.java
+++ app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/adapter/PrimeTableAdapterTest.java
@@ -1,11 +1,45 @@
 package uk.org.cobaltdevelopment.prime.web.adapter;
 
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import uk.org.cobaltdevelopment.prime.PrimeTable;
+import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
 
 public class PrimeTableAdapterTest {
 
+	private PrimeTableAdapter adapter;
+
+	@Before
+	public void setup() {
+		adapter = new PrimeTableAdapter();
+	}
+
 	@Test
-	public void canCreateAdapter(){
-		PrimeTableAdapter adapter = new PrimeTableAdapter();
+	public void canAdaptPrimeTableToDto() {
+		PrimeTable primeTableMock = Mockito.mock(PrimeTable.class);
+		when(primeTableMock.numberOfRows()).thenReturn(3);
+		when(primeTableMock.numberOfCols()).thenReturn(3);
+
+		when(primeTableMock.getCell(0, 0)).thenReturn(0);
+		when(primeTableMock.getCell(0, 1)).thenReturn(10);
+		when(primeTableMock.getCell(0, 2)).thenReturn(0);
+
+		when(primeTableMock.getCell(1, 0)).thenReturn(1);
+		when(primeTableMock.getCell(1, 1)).thenReturn(100);
+		when(primeTableMock.getCell(1, 2)).thenReturn(1);
+
+		when(primeTableMock.getCell(2, 0)).thenReturn(2);
+		when(primeTableMock.getCell(2, 1)).thenReturn(9999);
+		when(primeTableMock.getCell(2, 2)).thenReturn(1000);
+
+		TableDto result = adapter.convert(primeTableMock);
+
+		assertThat(result, Matchers.notNullValue());
 	}
 }
