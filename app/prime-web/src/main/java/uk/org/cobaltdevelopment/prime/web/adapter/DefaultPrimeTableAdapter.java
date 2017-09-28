ade6b5260c2279b2048f03fb3ac44f6b6d681f32
Chris Edgar <crsedgar@hotmail.com>
2017-09-28 15:55:03 +0100

    canAdaptPrimeTableToDto - refactor

--- app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/adapter/DefaultPrimeTableAdapter.java
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/adapter/DefaultPrimeTableAdapter.java
@@ -1,14 +1,20 @@
 package uk.org.cobaltdevelopment.prime.web.adapter;
 
 import uk.org.cobaltdevelopment.prime.PrimeTable;
+import uk.org.cobaltdevelopment.prime.web.dto.RowDto;
 import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
+import uk.org.cobaltdevelopment.prime.web.dto.TableDto.TableDtoBuilder;
 
 public class DefaultPrimeTableAdapter implements PrimeTableAdapter {
 
 	@Override
 	public TableDto convert(PrimeTable table) {
-		TableDto result = new TableDto();
-		return result;
+		TableDtoBuilder builder = TableDtoBuilder.create();
+		for (int i = 0; i < table.numberOfRows(); i++) {
+			builder.row(new RowDto());
+		}
+
+		return builder.build();
 	}
 
 }
--- app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/dto/TableDto.java
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/dto/TableDto.java
@@ -1,11 +1,46 @@
 package uk.org.cobaltdevelopment.prime.web.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TableDto {
 
-	public List<RowDto> getRows() {
-		return null;
+	private List<RowDto> rows;
+
+	public TableDto(List<RowDto> rows) {
+		super();
+		this.rows = rows;
 	}
 
+	public List<RowDto> getRows() {
+		return rows;
+	}
+
+	public static class TableDtoBuilder {
+
+		private List<RowDto> rows;
+
+		private TableDtoBuilder() {
+			rows = new ArrayList<>();
+		}
+
+		public static TableDtoBuilder create() {
+			return new TableDtoBuilder();
+		}
+
+		public TableDtoBuilder row(RowDto value) {
+			rows.add(value);
+			return this;
+		}
+
+		public TableDtoBuilder rows(List<RowDto> value) {
+			rows.addAll(value);
+			return this;
+		}
+
+		public TableDto build() {
+			TableDto content = new TableDto(rows);
+			return content;
+		}
+	}
 }
