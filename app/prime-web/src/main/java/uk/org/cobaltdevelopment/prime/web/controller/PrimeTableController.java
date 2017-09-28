1f50f46e834e005d59f9b36a69575c5b23fa9351
Chris Edgar <crsedgar@hotmail.com>
2017-09-28 18:37:37 +0100

    canAdaptPrimeTableToDto - green

--- app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableController.java
+++ app/prime-web/src/main/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import uk.org.cobaltdevelopment.prime.PrimeTable;
+import uk.org.cobaltdevelopment.prime.web.adapter.PrimeTableAdapter;
 import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
 import uk.org.cobaltdevelopment.prime.web.service.PrimeNumberService;
 
@@ -11,19 +12,19 @@ import uk.org.cobaltdevelopment.prime.web.service.PrimeNumberService;
 public class PrimeTableController {
 
 	private PrimeNumberService service;
+	private PrimeTableAdapter adapter;
 
 	@Autowired
-	public PrimeTableController(PrimeNumberService service) {
+	public PrimeTableController(PrimeNumberService service, PrimeTableAdapter adapter) {
 		super();
 		this.service = service;
+		this.adapter = adapter;
 	}
-
-
-
 
 	public TableDto generateTable(int numberOfPrimes) {
 		PrimeTable primeTable = service.createPrimeTable(numberOfPrimes);
-		return null;
+		TableDto tableDto = adapter.convert(primeTable);
+		return tableDto;
 	}
 
 }
--- app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableControllerTest.java
+++ app/prime-web/src/test/java/uk/org/cobaltdevelopment/prime/web/controller/PrimeTableControllerTest.java
@@ -1,42 +1,46 @@
 package uk.org.cobaltdevelopment.prime.web.controller;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.hamcrest.Matchers;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import uk.org.cobaltdevelopment.prime.PrimeTable;
+import uk.org.cobaltdevelopment.prime.web.adapter.PrimeTableAdapter;
 import uk.org.cobaltdevelopment.prime.web.dto.TableDto;
 import uk.org.cobaltdevelopment.prime.web.service.PrimeNumberService;
 
 public class PrimeTableControllerTest {
 
 	private PrimeNumberService serviceMock;
+	private PrimeTableAdapter adapterMock;
 
 	@Before
 	public void setUp() throws Exception {
 		serviceMock = Mockito.mock(PrimeNumberService.class);
+		adapterMock = Mockito.mock(PrimeTableAdapter.class);
 	}
 
-	@Test
-	public void canCreateController() {
-		new PrimeTableController(serviceMock);
-	}
-
-	@Ignore
 	@Test
 	public void canCreateTable() {
-		PrimeTable value = Mockito.mock(PrimeTable.class);
-		Mockito.when(serviceMock.createPrimeTable(2)).thenReturn(value);
+		PrimeTable tableMock = Mockito.mock(PrimeTable.class);
+		TableDto tableDtoMock = Mockito.mock(TableDto.class);
 
-		PrimeTableController controller = new PrimeTableController(serviceMock);
+		when(serviceMock.createPrimeTable(2)).thenReturn(tableMock);
+		when(adapterMock.convert(tableMock)).thenReturn(tableDtoMock);
+
+		PrimeTableController controller = new PrimeTableController(serviceMock, adapterMock);
 		TableDto t = controller.generateTable(2);
 
-		Mockito.verify(serviceMock, Mockito.only()).createPrimeTable(2);
-		assertThat(t, Matchers.notNullValue());
+		verify(serviceMock, only()).createPrimeTable(2);
+		verify(adapterMock, only()).convert(Mockito.any());
+
+		assertThat(t, notNullValue());
 	}
 
 }
